4e5b635684721000c28005682948f67b50242487
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-19 10:22:07 +0000

    progress

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar'
 
 function Navbar() {
   return (
-    <nav className='fixed w-full h-[10vh] m-1 p-5 border border-black flex justify-between items-center bg-white z-50'>
+    <nav className='fixed w-full h-[10vh] p-5 border border-black flex justify-between items-center bg-white z-50'>
         {/* Logo */}
         <section>
             <p className='text-clash font-bold text-2xl'>nextcommerce</p>
