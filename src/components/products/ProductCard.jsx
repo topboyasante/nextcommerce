52bca9b15b8e3194ab4ad78ff5f2361893307713
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-24 12:54:22 +0000

    changes

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -5,7 +5,7 @@ function ProductCard({img,name,price,brand,slug}) {
   return (
     <Link href={slug}>
       <main className='border border-black hover:scale-95 ease duration-500 bg-white '>
-          <img src={img} alt={name} className='w-full h-[700px] mx-auto'/>
+          <img src={img} alt={name} className='w-full h-[380px] lg:h-[700px] mx-auto'/>
             <section className='text-clash text-center p-5'>
               <hr className='my-5' />
               <p className='font-semibold lg:text-[0.8em] uppercase tracking-wider truncate'>{name}</p>
